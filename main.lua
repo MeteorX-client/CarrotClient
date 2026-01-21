7e4e93faea246ed6fa7b635a30d6b2db7a77805c
MeteorX <99117606+MeteorX-client@users.noreply.github.com>
2026-01-21 17:08:03 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,5 @@
-print("Coming")
+if game.PlaceId == 155615604 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/MeteorX-client/CarrotClient/refs/heads/main/PrisonLifeCarrot.lua"))()
+else
+  game.Players.LocalPlayer:Kick("Map Not Found")
+end
